c2f0538ecd1fac1cba12d034b45bc59de170f513
Alefer97 <ferriero.1758213@studenti.uniroma1.it>
2019-03-20 16:55:17 +0100

    Added bitmap

---
+++ allocator/bit_map.h
@@ -0,0 +1,15 @@
+#include <stdint.h>
+
+typedef struct{
+  int size;// grandezza buffer
+  int bits;// numero tot di bit
+  uint8_t *friendo; // buffer per il buddy
+}BitMap;
+
+int Bit_GetBytesFromBit(int n_bits);// converte bit in byte.
+
+void Bit_init(BitMap* bm,int n_size,uint8_t* buffo); //inizializzazione
+
+int Bit_status( BitMap*bm,int index); //funzione fondamentale di controllo
+
+void Bit_setBit(BitMap*bm,int index, int situa);//setta un bit a 0 o a 1;
