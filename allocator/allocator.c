3185484bab7cf4deec1f68fb083270aab7ad5d70
Sterminator0204 <gamatteo1998@libero.it>
2019-03-22 10:49:08 +0100

    allocator funzionante, da fare ultimi test

--- allocator/allocator.c
+++ allocator/allocator.c
@@ -6,7 +6,7 @@
 
 typedef struct {
   int* memory;
-  int size;
+  int block_size;
   int num;
   int*check;
 } buffer;
@@ -16,13 +16,13 @@ typedef struct {
 void* my_alloc(buffer* buff, int size){
   int i = 0;
   int j = 0;
-  int numbers = size/dim;
+  int numbers = size/(buff->block_size);
   if(size%dim != 0) numbers++;
-  while(i++<(buff->num)-numbers){
+  for(; i <= (buff->num)-numbers; i++){
     if(buff->check[i] == 0){
-      int s = i;
-      for(; s++; s < numbers){
-        buff->check[s] == 1;
+      int s;
+      for(s = 0; s < numbers; s++){
+        buff->check[i+s] = 1;
       }
       return &(buff->memory[i]);
     }
@@ -34,14 +34,16 @@ void* my_alloc(buffer* buff, int size){
 int main(){
   int mem[max];
   buffer buf ;
+  buf.block_size = dim;
   buf.memory = mem;
   buf.num = max/dim;
   int check1[max/dim];
   int j;
-    for(j = 0; j++; j < buf.num) check1[j] = 0;
+    for(j = 0; j < buf.num; j++) check1[j] = 0;
   buf.check = check1;
-  int* prova1 = (int*)my_alloc(&buf, 32);
-  *prova1 = 5;
-  printf("%d\n", *prova1);
-
+  int l;
+  buffer* buf_prova = (buffer*)my_alloc(&buf, sizeof(buffer));
+  *buf_prova = buf;
+  printf("%d\n", buf.num);
+  printf("%d\n", buf_prova->num);
 }
