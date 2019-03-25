0cd7275881a8708f33e9dc2008541a9ff003dd81
Leonardo Campitelli <46381897+Campo97@users.noreply.github.com>
2019-03-25 16:30:31 +0100

    Test Makefile

--- allocator/bit_map.c
+++ allocator/bit_map.c
@@ -7,7 +7,7 @@ void BitMap_init(BitMap* b, int size){
   b -> friendo = (unsigned char*) malloc(size*sizeof(unsigned char));
   b -> size = size;
   b -> bits = size*8;
-  BitMap_setRange(&bm, 0, 0, bm.bits);
+  BitMap_setRange(b, 0, 0, b->bits);
 }
 
 void BitMap_set(BitMap* b, int i) {
