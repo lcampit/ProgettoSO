65ea762a8c2b3add7dcfcc2fd7c93ce8ed3fcc1c
Alefer97 <48677018+Alefer97@users.noreply.github.com>
2019-04-17 17:56:53 +0200

    Modified allocator.c , now it behaves like a heap

--- allocator/allocator.c
+++ allocator/allocator.c
@@ -1,49 +1,68 @@
 #include <stdlib.h>
 #include <stdio.h>
-
-#define dim 4
-#define max 12800
-
-typedef struct {
-  int* memory;
-  int block_size;
-  int num;
-  int*check;
-} buffer;
-
-
+#include <math.h>
+#include "allocator.h"
 
 void* my_alloc(buffer* buff, int size){
-  int i = 0;
-  int j = 0;
-  int numbers = size/(buff->block_size);
-  if(size%dim != 0) numbers++;
-  for(; i <= (buff->num)-numbers; i++){
-    if(buff->check[i] == 0){
-      int s;
-      for(s = 0; s < numbers; s++){
-        buff->check[i+s] = 1;
-      }
-      return &(buff->memory[i]);
-    }
-  }
-  printf("not enough memory\n");
-  return NULL;
+	int level= buddylevel(buff,size);
+	printf("%d\n", size);
+	printf("level %d\n", level);
+	int levello = level;
+	int k = 0;
+	int i=pow(2,level);
+	int stop=pow(2,level+1);
+	for(;i<stop;i++){
+		if(BitMap_get(buff->check,i)==0){
+			int j= i/2;
+			while(level){
+				//printf("%d\n",j);
+				if(BitMap_get(buff->check,j)==1) break;
+				BitMap_set(buff->check,j);
+				level--;
+				j=j/2;
+			}
+			set_children(buff,i,levello);
+			int num = buff->max/(pow(2, levello));
+			return (void*)&(buff->memory[num*k*buff->block_size]);
+		}
+		k++;
+	}
+	  
 }
 
-int main(){
-  int mem[max];
-  buffer buf ;
-  buf.block_size = dim;
-  buf.memory = mem;
-  buf.num = max/dim;
-  int check1[max/dim];
-  int j;
-    for(j = 0; j < buf.num; j++) check1[j] = 0;
-  buf.check = check1;
-  int l;
-  buffer* buf_prova = (buffer*)my_alloc(&buf, sizeof(buffer));
-  *buf_prova = buf;
-  printf("%d\n", buf.num);
-  printf("%d\n", buf_prova->num);
+
+/*void my_free(buffer* b, void* ptr, int size){
+  int ind = ptr;
+  int start = &(b->memory[0]);
+  int number = (ind - start)/4;
+  int i;
+  int num = size/b->block_size;
+  for(i = 0; i < num; i++){
+    BitMap_unset(b->check, number+i);
+  }
+*/
+
+int buddylevel(buffer* buff,int size){ 
+     if(size>buff->max){
+	fprintf(stderr,"not enough memory!\n");
+	exit(1);
+}
+     int pow=0;
+     int aux=buff->max;
+     aux=aux/2;
+	while (size<=aux){
+		pow++;
+		aux=aux/2;
+    }
+	return pow;
+	
+}
+
+void set_children(buffer*buf,int i,int level){
+	BitMap_set(buf->check,i);
+	if(level==buf->levels) return;
+	else{
+		set_children(buf,2*i,level+1);
+		set_children(buf,2*i+1,level+1);
+	}
 }
