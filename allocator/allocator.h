dd6c2e6452975743350dbf3091d4c442970a7dd3
Alefer97 <48677018+Alefer97@users.noreply.github.com>
2019-04-25 17:51:18 +0200

    added latest auxiliary functions

--- allocator/allocator.h
+++ allocator/allocator.h
@@ -11,6 +11,8 @@ typedef struct {
 }buffer;
 
 void* my_alloc(buffer* buff, int size);//AF,MG grants memory spaces
-void my_free(buffer* b, void* ptr,int size);//AF,MG  frees memory TODO (we have the general algorithm, not the time)
+void my_free(buffer* b, void* ptr,int size);//AF,MG  frees memory 
 int buddylevel(buffer* buff,int size); //AF,MG returns level of depth of minimum memory segment
 void set_children(buffer*buf,int i,int level); //AF,MG sets children in bitmap
+void unset_children(buffer*buf,int i,int level); //AF,MG unsets children in bitmap
+void unset_parents(buffer*buf,int i,int level); //AF,MG unsets parents in bitmap until buddies aren't free anymore
