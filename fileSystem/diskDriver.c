aebe46715a7a82ce68554222529b10cc153cfb92
Francesca Fiani <vivitheblackmage98@gmail.com>
2019-05-12 09:06:52 -0400

    Disk Driver completed, updated fileSystem.h

---
+++ fileSystem/diskDriver.c
@@ -0,0 +1,61 @@
+#include "diskDriver.h"
+
+void DiskDriver_init(DiskDriver* disk, int num_blocks, buffer* buf) {
+  FirstDirectoryBlock* memo = my_alloc(buf, sizeof(FirstDirectoryBlock)*num_blocks*BLOCK_SIZE);
+  disk->mem=memo;
+
+  BitMap bit;
+  BitMap_init(&bit, NUM_BLOCK);
+
+  DiskHeader* head = my_alloc(buf, sizeof(DiskHeader));
+  head->num_blocks = NUM_BLOCK;
+  head->bitmap_entries = NUM_BLOCK/8;
+  head->free_blocks = NUM_BLOCK;
+  head->first_free_block = 1;
+
+  FirstDirectoryBlock* root = my_alloc(buf, sizeof(FirstDirectoryBlock));   //allocation of first directory "/"
+  root->num_entries = 0;
+  int* a = my_alloc(buf, (BLOCK_SIZE-sizeof(BlockHeader)-sizeof(FileControlBlock)-sizeof(int))/sizeof(int));
+  root-> file_blocks = a;
+  BlockHeader* blockh = my_alloc(buf, sizeof(BlockHeader));
+  blockh->previous_block = -1;
+  blockh->next_block = -1
+  blockh->block_in_file = 0;
+  root->header = blockh;
+  FileControlBlock* ficb = my_alloc(buf, sizeof(FileControlBlock));
+  ficb->directory_block=-1;
+  ficb->block_in_disk=0;
+  ficb->name="/";
+  ficb->size_in_bytes = sizeof(FirstDirectoryBlock);
+  ficb->size_in_blocks=1;
+  ficb->is_dir=1;
+  root->fcb=ficb;
+  disk->mem[0] = root;
+  return;
+}
+
+int DiskDriver_readBlock(DiskDriver* disk, void* dest, int block_num){
+  *dest = disk->mem[block_num];
+  int check = BitMap_get(disk->bitmap, block_num);
+  if (check==0) return -1;
+  else return 0;
+}
+
+int DiskDriver_writeBlock(DiskDriver* disk, void* src, int block_num){
+  if (block_num>NUM_BLOCK || block_num==0) return -1;
+  disk->mem[block_num]=src;
+  BitMap_set(disk->bitmap, block_num);
+  return 0;
+}
+
+int DiskDriver_freeBlock(DiskDriver* disk, int block_num) {
+  if (block_num>NUM_BLOCK || block_num==0) return -1;
+  disk->mem[block_num] = NULL;
+  BitMap_unset(disk->bitmap, block_num);
+  return 0;
+}
+
+int DiskDriver_getFreeBlock(DiskDriver* disk, int start){
+  if (start>disk->header->first_free_block) return start;
+  else return disk->header->first_free_block;
+}
---
+++ utilities/linked_list/test.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include "linked_list.h"
+
+#define MAX_NUM_ITEMS 64
+
+typedef struct IntlistItem{
+  listItem list;
+  int info;
+} IntlistItem;
+
+void Intlist_print(listHead* head){
+  listItem* aux=head->first;
+  printf("[");
+  while(aux){
+    IntlistItem* element = (IntlistItem*) aux;
+    printf("%d ", element->info);
+    aux=aux->next;
+  }
+  printf("]\n");
+}
+
+int main(int argc, char** argv) {
+  // we populate the list, by inserting MAX_NUM_ITEMS
+  listHead head;
+  list_init(&head);
+  for (int i=0; i<MAX_NUM_ITEMS; ++i){
+    IntlistItem* new_element= (IntlistItem*)
+      malloc(sizeof(IntlistItem));
+    if (! new_element) {
+      printf("out of memory\n");
+      break;
+    }
+    new_element->list.prev=0;
+    new_element->list.next=0;
+    new_element->info=i;
+    listItem* result=
+      list_insert(&head, head.last, (listItem*) new_element);
+    assert(result);
+  }
+  Intlist_print(&head);
+
+  printf("removing odd elements");
+  listItem* aux=head.first;
+  int k=0;
+  while(aux){
+    listItem* item=aux;
+    aux=aux->next;
+    if (k%2){
+      list_detach(&head, item);
+      free(item);
+    }
+    ++k;
+  }
+  Intlist_print(&head);
+
+  printf("removing from the head, half of the list");
+  int size=head.size;
+  k=0;
+  while(head.first && k<size/2){
+    listItem* item=list_detach(&head, head.first);
+    assert(item);
+    free(item);
+    ++k;
+  }
+  Intlist_print(&head);
+
+  printf("removing from the tail the rest of the list, until it has 1 element");
+  while(head.first && head.size>1){
+    listItem* item=list_detach(&head, head.last);
+    assert(item);
+    free(item);
+  }
+
+  Intlist_print(&head);
+
+  printf("removing last element");
+  listItem* item=list_detach(&head, head.last);
+  assert(item);
+  free(item);
+  Intlist_print(&head);
+
+}
