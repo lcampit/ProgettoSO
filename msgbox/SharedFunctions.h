e1a5c972e16174b0e13f8d63795e491029e4ff69
Sterminator0204 <gamatteo1998@libero.it>
2019-07-23 16:07:34 +0200

    rifiniture

--- msgbox/SharedFunctions.h
+++ msgbox/SharedFunctions.h
@@ -8,7 +8,10 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <semaphore.h>
+#include<errno.h>
+#include <signal.h>
 
-void* SharedCreate(char* name, int size, int flag); //crea la shm e la mmappa per tutti i processi che ne hanno bisogno, viene chiamata UNA volta AF MG FF
-int SharedWrite(char* text, void* memory); //scrive nella shm il messaggio, restituendo la lunghezza dello stesso, in modo che possa essere aggiunta al puntatore nel processo AF MG FF
-int SharedRead(void* memory); // legge il contenuto della shm AF MG FF
+void* SharedCreate(char* name,int size,int flag); //crea la shm e la mmappa per tutti i processi che ne hanno bisogno,viene chiamata UNA volta AF MG FF
+int SharedWrite(char* text, void* memory); // scrive nella memoria condivisa il messaggio, restituendo per altro la lunghezza del messaggio, in modo che possa essere aggiunta al puntatore nel processo AF MG FF
+int SharedRead(void*memory); // legge il contenuto della memoria AF MG FF
